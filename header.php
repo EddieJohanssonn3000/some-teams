fdbadd9b825717320ee219540a175f4314b64c80
Eddie Johansson <eddiejohansson@MacBook-Air-som-tillhor-Eddie.local>
2025-10-02 10:25:44 +0200

    second commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ultimate Football Teams</title>
+</head>
+<body>
+    <h1>UFT - Ultimate Football Teams</h1>
+    <nav>
+        <a href="index.php">Start</a>
+        <a href="about.php">About</a>
+    </nav>
+</body>
+</html>
